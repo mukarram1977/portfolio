dc9c4e2d697a3fd180201b7a80651076e51b802c
unknown <hmmukarram58@gmail.com>
2024-11-01 11:31:45 -0700

    portfolio

---
+++ index-2.js
@@ -0,0 +1,126 @@
+(function ($) {
+    "use strict";
+    // Initiate the wowjs
+    new WOW().init();
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 90) {
+            $('.nav-bar').addClass('sticky-top shadow');
+        } else {
+            $('.nav-bar').removeClass('sticky-top shadow');
+        }
+    });
+    // Dropdown on mouse hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+    
+    $(window).on("load resize", function() {
+        if (this.matchMedia("(min-width: 992px)").matches) {
+            $dropdown.hover(
+            function() {
+                const $this = $(this);
+                $this.addClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "true");
+                $this.find($dropdownMenu).addClass(showClass);
+            },
+            function() {
+                const $this = $(this);
+                $this.removeClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "false");
+                $this.find($dropdownMenu).removeClass(showClass);
+            }
+            );
+        } else {
+            $dropdown.off("mouseenter mouseleave");
+        }
+    });
+    /*** Get in Touch Start here **/
+    $(document).ready(function () {
+        $('#contactForm').on('submit', function (e) {
+          e.preventDefault(); // Prevent form submission
+      
+          // Capture form data  /*** Get in Touch Start here **/
+          const name = $('#name').val();
+          const email = $('#email').val();
+          const subject = $('#subject').val();
+          const message = $('#message').val();
+      
+          // Clear the form fields  /*** Get in Touch Start here **/
+          $('#contactForm')[0].reset();
+      
+          // Display a success message  /*** Get in Touch Start here **/
+          $('#responseMessage').text(`Thank you, ${name}! Your message has been sent.`);
+        });
+      });
+       /*** Get in Touch End here **/
+      
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+    
+
+   // Testimonials carousel
+   $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    dots: false,
+    loop: true,
+    nav : true,
+    navText : [
+        '<i class="bi bi-arrow-left"></i>',
+        '<i class="bi bi-arrow-right"></i>'
+    ],
+    responsive: {
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+});
+})(jQuery);
